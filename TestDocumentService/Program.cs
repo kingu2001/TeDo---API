931ff62c144769947d362b5d295c89460eca5f89
HasseG <hassegregers@gmail.com>
2024-02-22 16:41:57 +0100

    Still messing with CORS

--- TestDocumentService/Program.cs
+++ TestDocumentService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Any;
 using TestDocumentService.Data;
 using TestDocumentService.Data.Context;
 using TestDocumentService.Data.DatabaseSeed;
@@ -6,6 +7,12 @@ using TestDocumentService.Data.Interfaces;
 using TestDocumentService.Data.Repositorys;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var MyAllowPolicy = "_myAllowPolicy";
+builder.Services.AddCors(opt => opt.AddPolicy(name: MyAllowPolicy, policy =>
+{
+    policy.WithOrigins("https://google.com", "https://wowhead.com").AllowAnyMethod().AllowAnyHeader();
+}));
 
 // Add services to the container.
 
@@ -16,6 +23,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//For testing ONLY!!
+// Console.WriteLine("--> Enter origin to allow with CORS");
+// var origin = Console.ReadLine();
+// Console.WriteLine($"--> Entered origin: {origin}");
+
 
 //Dependeny Injection
 builder.Services.AddScoped<ITestDocumentRepo, TestDocumentRepo>();
@@ -31,9 +44,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(MyAllowPolicy);
+
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 PrepDb.PrepPopulation(app);
 
