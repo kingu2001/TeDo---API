4245d8ba8aa18e30ddedc98c7e63ec277396da5c
HasseG <hassegregers@gmail.com>
2024-04-02 14:32:51 +0200

    Created new converting service

---
+++ ConverterService/ConverterService/Controllers/ConverterController.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+using Microsoft.AspNetCore.Mvc;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
+
+namespace ConverterService
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConverterController : ControllerBase
+    {
+        public ConverterController()
+        {
+        
+        }
+
+        [HttpPost]
+        public async Task<IActionResult<Document>> ConvertDocxToPdf(Document document)
+        {
+            
+            Stream stream = new MemoryStream(document.FileContent);
+
+            WordDocument wordDocument = new WordDocument(stream, Syncfusion.DocIO.FormatType.Docx, XHTMLValidationType.None);
+
+            DocIORenderer render = new DocIORenderer();
+            //Converts Word document to PDF.
+            PdfDocument pdfDocument = render.ConvertToPDF(wordDocument);
+            //Release the resources used by the Word document and DocIO Renderer objects.
+
+            
+            render.Dispose();
+            wordDocument.Dispose();
+        }
+    }
+}
---
+++ ConverterService/ConverterService/Model/Document.cs
@@ -0,0 +1,9 @@
+﻿namespace ConverterService;
+
+public class Document
+{
+    public int Id { get; set; }
+    public string? FileName { get; set; }
+    public string? ContentType { get; set; } 
+    public byte[]? FileContent { get; set; }
+}
